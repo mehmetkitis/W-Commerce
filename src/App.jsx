354687564cd537cdf3767f1b445d11ae4ae02ff5
Mehmet Kitiş <103045822+mehmetkitis@users.noreply.github.com>
2024-12-26 17:43:22 +0300

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { toast, ToastContainer } from 'react-toastify'
+import { Camera } from 'lucide-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const toast1 = () => {
+    toast.error("NO");
+  }
+  const countChanger = () => {
+    setCount(count + 1);
+  }
+  const btnOnClick = () => {
+    toast1();
+    countChanger();
+
+  }
 
   return (
     <>
@@ -18,9 +31,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='text-green-600' onClick={btnOnClick}>
           count is {count}
         </button>
+        <Camera color='red' size={48} />
+
+        <ToastContainer />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
