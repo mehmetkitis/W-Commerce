092b4d6f34e0bfb2335ca800bc7c8e65775ba6b4
Mehmet Kitiş <103045822+mehmetkitis@users.noreply.github.com>
2024-12-28 13:33:37 +0300

    css

---
+++ src/assets/assets.js
@@ -0,0 +1,5 @@
+import product1 from "./product-slide-1.jpg"
+
+export const assets = {
+    product1,
+}
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,12 @@
 import { AlignRight, Search, ShoppingCart, User } from 'lucide-react'
 import React from 'react'
+import { assets } from '../assets/assets'
+
+
 
 const Header = () => {
     return (
-        <div className='h-[1170px]'>
+        <div className='m-0'>
             <div className='h-[532px]'>
                 <div className='flex justify-between m-0 '>
                     <p className='font-bold '>Bandage</p>
@@ -15,14 +18,15 @@ const Header = () => {
                     </div>
                 </div>
                 <div className='flex flex-col mt-24 gap-10 font-semibold text-[#737373] text-4xl justify-between'>
-                    <button>Home</button>
+                    <button >Home</button>
                     <button>Product</button>
                     <button>Pricing</button>
                     <button>Contact</button>
                 </div>
             </div>
 
-            <div className='bg-productImg1 h-[638px] bg-no-repeat mt-10 w-[414px]'>
+            <div className='h-[640px] '>
+                <img src={assets.product1} className=' h-full w-screen' />
                 <p className='text-white text-5xl pt-48'>GROCERIES DELIVERY</p>
                 <p className='text-white text-[1.8rem] m-8'>We know how large objects will act, but things on a small scale.</p>
                 <button className='bg-sky-500 text-white p-2 font-semibold pr-7 pl-7 rounded-md'>Start Now</button>
